31ebe39711db784c588d6166c0dfc81861e5db63
anishsingh@20 <loveeanish@gmail.com>
2018-03-08 23:27:45 +0530

    rad the dataset in R

--- Autoencoder.R
+++ Autoencoder.R
@@ -1,1 +1,9 @@
 #Credit card fraud detection using a AUTOENCODER And KERAS in R
+require(dplyr)
+require(ggplot2)
+require(readr)#faster data reading
+
+#reading the data
+
+fraud<-read_csv("../Datasets/creditcardfraud/creditcard.csv", 
+                col_types = list(Time = col_number()))
