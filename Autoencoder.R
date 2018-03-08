d60af88593cb72b12eea49885c59ca0c5c24bb34
anishsingh@20 <loveeanish@gmail.com>
2018-03-08 23:37:13 +0530

    added description about the dataset

--- Autoencoder.R
+++ Autoencoder.R
@@ -12,10 +12,14 @@ fraud<-read_csv("../Datasets/creditcardfraud/creditcard.csv",
 #We only have Time,Amount and Class label(0=non-fradulent, 1=fradulent) as variables. 
 
 
+#Feature 'Time' contains the seconds elapsed between each transaction and the first transaction in the dataset. 
+#The feature 'Amount' is the transaction Amount.
+
 #checking out the data
 glimpse(fraud)
 attach(fraud)
 
 table(Class)
 #highly unbalanced dataset- only 492 fradulent cases
+
 
